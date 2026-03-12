1e2e086d047c4b614c83b1db20146c87ef49214e
RRR <patrarahul989@google.com>
2026-03-13 03:57:21 +0530

    New branch

---
+++ frontend/src/pages/Flashcards/FlashcardsListPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FlashcardsListPage = () => {
+  return (
+    <div>
+      <h1>Flashcards List Page...</h1>
+    </div>
+  )
+}
+
+export default FlashcardsListPage
