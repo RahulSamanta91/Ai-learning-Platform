4ad7a22b6619f2500b42488b52b0897010a9b603
Rahul-65-Patra <patrarahul989@gmail.com>
2026-03-21 22:33:21 +0530

    Fix CORS origin

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,10 @@ connectDB();
 // Middleware to handles CORS
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:5173",
+      "https://smartlearnlearningplatform.vercel.app"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
